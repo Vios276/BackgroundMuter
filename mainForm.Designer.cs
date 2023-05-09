3d55bb42e5994aa48106c452a83e4876bcd89ac8
Vios276 <vios276@naeilsg.com>
2023-05-09 21:22:28 +0900

    초기 커밋.

---
+++ mainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace BackgroundMuter
+{
+    partial class BackgroundMuter
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BackgroundMuter));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ToolStripMenuItem_version = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem_AutoStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ToolStripMenuItem_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_version,
+            this.ToolStripMenuItem_AutoStart,
+            this.toolStripSeparator1,
+            this.ToolStripMenuItem_Exit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 76);
+            // 
+            // ToolStripMenuItem_version
+            // 
+            this.ToolStripMenuItem_version.Enabled = false;
+            this.ToolStripMenuItem_version.Name = "ToolStripMenuItem_version";
+            this.ToolStripMenuItem_version.Size = new System.Drawing.Size(210, 22);
+            this.ToolStripMenuItem_version.Text = "버젼";
+            // 
+            // ToolStripMenuItem_AutoStart
+            // 
+            this.ToolStripMenuItem_AutoStart.Checked = true;
+            this.ToolStripMenuItem_AutoStart.CheckOnClick = true;
+            this.ToolStripMenuItem_AutoStart.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ToolStripMenuItem_AutoStart.Name = "ToolStripMenuItem_AutoStart";
+            this.ToolStripMenuItem_AutoStart.Size = new System.Drawing.Size(210, 22);
+            this.ToolStripMenuItem_AutoStart.Text = "윈도우 시작 시 자동 실행";
+            this.ToolStripMenuItem_AutoStart.CheckStateChanged += new System.EventHandler(this.ToolStripMenuItem_AutoStart_CheckStateChanged);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(207, 6);
+            // 
+            // ToolStripMenuItem_Exit
+            // 
+            this.ToolStripMenuItem_Exit.Name = "ToolStripMenuItem_Exit";
+            this.ToolStripMenuItem_Exit.Size = new System.Drawing.Size(210, 22);
+            this.ToolStripMenuItem_Exit.Text = "종료";
+            this.ToolStripMenuItem_Exit.Click += new System.EventHandler(this.ToolStripMenuItem_Exit_Click);
+            // 
+            // BackgroundMuter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 260);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BackgroundMuter";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BackgroundMuter_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.BackgroundMuter_FormClosed);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_version;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_AutoStart;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Exit;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Deployment.Application;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace BackgroundMuter
+{
+    public partial class BackgroundMuter : Form
+    {
+        private int _targetId;
+        private readonly RegistryKey _key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        public BackgroundMuter()
+        {
+            InitializeComponent();
+            Init();
+            VolumeMixer.SetApplicationMute(_targetId, GetActiveProcessFileName() != "StarRail");
+            StartListeningForWindowChanges();
+        }
+
+        private void Init()
+        {
+            Visible = false;
+
+            _targetId = (from process in Process.GetProcesses()
+                where process.ProcessName == "StarRail" && !string.IsNullOrEmpty(process.MainWindowTitle)
+                select process.Id).FirstOrDefault();
+
+            ToolStripMenuItem_AutoStart.Checked = _key.GetValue(Name) != null;
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                var intMajor = ApplicationDeployment.CurrentDeployment.CurrentVersion﻿.Major;
+                var intMinor = ApplicationDeployment.CurrentDeployment.CurrentVersion﻿.Minor;
+                var intBuild = ApplicationDeployment.CurrentDeployment.CurrentVersion﻿.Build;
+                ToolStripMenuItem_version.Text = $@"Version : {intMajor}.{intMinor}.{intBuild}";
+            }
+            else
+            {
+                ToolStripMenuItem_version.Text = $@"Develop Build";
+            }
+            
+        }
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc,
+            WinEventProc lpfnWinEventProc, int idProcess, int idThread, uint dwflags);
+
+        [DllImport("user32.dll")]
+        private static extern int UnhookWinEvent(IntPtr hWinEventHook);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        private delegate void WinEventProc(IntPtr hWinEventHook, uint iEvent, IntPtr hWnd, int idObject, int idChild,
+            int dwEventThread, int dwmsEventTime);
+
+        private static string GetActiveProcessFileName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process p = Process.GetProcessById((int)pid);
+            return p.ProcessName;
+        }
+
+        const uint WINEVENT_OUTOFCONTEXT = 0;
+        const uint EVENT_SYSTEM_FOREGROUND = 3;
+        private IntPtr winHook;
+        private WinEventProc listener;
+
+        private void StartListeningForWindowChanges()
+        {
+            listener = EventCallback;
+            //setting the window hook
+            winHook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, listener, 0, 0,
+                WINEVENT_OUTOFCONTEXT);
+        }
+
+        private void StopListeningForWindowChanges()
+        {
+            UnhookWinEvent(winHook);
+        }
+
+        private void EventCallback(IntPtr hWinEventHook, uint iEvent, IntPtr hWnd, int idObject, int idChild,
+            int dwEventThread, int dwmsEventTime)
+        {
+            VolumeMixer.SetApplicationMute(_targetId, GetActiveProcessFileName() != "StarRail");
+        }
+        
+        private void ToolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ToolStripMenuItem_AutoStart_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (ToolStripMenuItem_AutoStart.Checked)
+            {
+                _key.SetValue(Name,Application.ExecutablePath);
+            }
+            else
+            {
+                try
+                {
+                    _key.DeleteValue(Name);
+                }
+                catch
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        private void BackgroundMuter_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            VolumeMixer.SetApplicationMute(_targetId, false);
+        }
+
+        private void BackgroundMuter_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopListeningForWindowChanges();
+        }
+    }
+}
